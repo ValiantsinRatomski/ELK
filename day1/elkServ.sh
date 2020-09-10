e260000e47de2ec85c266cc655970bfd711ee6f1
Valiantsin Ratomski <Valiantsin_Ratomski@epam.com>
2020-09-10 20:53:33 +0300

    added task #1(elk)

---
+++ day1/elkServ.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+sudo su
+yum install java-11-openjdk-devel -y
+yum install wget -y
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+cat > /etc/yum.repos.d/logstash.repo << EOT
+[logstash-7.x]
+name=Elastic repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOT
+
+
+yum install logstash -y
+/usr/share/logstash/bin/system-install
+
+cat > /etc/logstash/conf.d/ls.conf << EOF
+input {
+  file {
+    path => “/path/to/log"
+    start_position => "beginning"
+  }
+}
+
+output {
+  elasticsearch {
+    hosts => ["localhost:9200"]
+  }
+  stdout { codec => rubydebug }
+}
+EOF
+
+systemctl start logstash
+
+#----------------------------------------------------------------------
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.1-x86_64.rpm
+sudo rpm --install elasticsearch-7.9.1-x86_64.rpm
+
+systemctl start elasticsearch.service
+
+#----------------------------------------------------------------------
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+cat > /etc/yum.repos.d/kibana.repo << EOT
+[kibana-7.x]
+name=Kibana repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOT
+
+yum install kibana -y
+echo -e '\nserver.port: 5601\nserver.host: "0.0.0.0"' >> /etc/kibana/kibana.yml
+systemctl start kibana
